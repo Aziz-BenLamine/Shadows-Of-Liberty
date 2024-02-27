da093b832247e598439fe8a0ebd2c91731d8eb8a
Aziz-BenLamine <azizbenlamine@gmail.com>
2024-02-27 22:28:07 +0100

    Add menu navigation with arrow keys

--- src/main.c
+++ src/main.c
@@ -15,24 +15,24 @@ int main(int argc, char** argv) {
     SDL_Surface *ecran;
     int playing = 1;
     int buttonClicked = 0;
+    int keysClicked = 0;
     int menuHoverSoundPlayed = 0;
     int delay = 0;
+    int selectedButtonIndex = 0;
+    int previousButtonIndex = 0;
 
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
         printf("Echec d'initialisation de SDL : %s\n", SDL_GetError());
         return 1;
     }
 
-    // INIT MUSIC
     if(Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 2048 ) == -1) {
         printf("Audio Error:%s",Mix_GetError());
     }
-    //INIT MENU BACKGROUND SOUND
+
     backgroundSound BS;
     InitBackgroundSound(&BS);
 
-
-    // INIT MENU HOVER SOUND
     Mix_Chunk *menuHoverSound;
     menuHoverSound = Mix_LoadWAV("../audio/menuHoverSound.wav");
     Mix_VolumeChunk(menuHoverSound, MIX_MAX_VOLUME / 2);
@@ -42,8 +42,7 @@ int main(int argc, char** argv) {
         fprintf(stderr, "ERROR CREATING THE WINDOW %d*%d: %s.\n", SCREEN_HEIGHT, SCREEN_WIDTH, SDL_GetError());
         return 1;
     }
-    
-    // INIT BACKGROUND
+
     Background background;
     InitBackground(&background);
 
@@ -52,141 +51,190 @@ int main(int argc, char** argv) {
         printf("ERROR INITIALIZING MENU BUTTONS.\n");
         return 3;
     }
-    
+
     button settingButtons[SETTING_BUTTONS_COUNT];
     if(InitSettingsButtons(settingButtons) != 0){
-    	printf("ERROR INITIALIZING MENU BUTTONS \n");
-    	return 4;
+        printf("ERROR INITIALIZING MENU BUTTONS \n");
+        return 4;
     }
-    
+
     SDL_Event event;
     while (playing) {
         AfficherBackground(background, ecran);
-        
+
         SDL_PollEvent(&event);
         switch (event.type) {
             case SDL_QUIT:
                 playing = 0;
                 break;
-            }
-            
-        //Main Menu Handling and Blitting
-        if(background.niveau == 0){
-        AfficherBouton(menuButtons, ecran, 0);
-	
-        SDL_PollEvent(&event);
-        switch (event.type) {
-            case SDL_QUIT:
-                playing = 0;
-                break;
-            case SDL_MOUSEMOTION:
-                for (int i = 0; i < MENU_BUTTONS_COUNT; i++) {
-                    if (point_in_rect(event.motion.x, event.motion.y, menuButtons[i].button_rect)) {
-                        if (menuButtons[i].actif == 0) {
-                            menuButtons[i].actif = 1;
-                            if (!menuHoverSoundPlayed) {
-                                Mix_PlayChannel(1, menuHoverSound, 0);
-                                menuHoverSoundPlayed = 1;
-                            }
-                        }
-                    } else {
-                        menuButtons[i].actif = 0;
-                    }
-                }
-                break;
-            case SDL_MOUSEBUTTONDOWN:
-                        if (event.button.button == SDL_BUTTON_LEFT) {
-                            if (point_in_rect(event.button.x, event.button.y, menuButtons[0].button_rect)) {
-                                background.niveau = 2;
-                                for (int i = 0; i < MENU_BUTTONS_COUNT; i++) {
-                                    menuButtons[i].actif = 0;
-                                }
-                            } else if (point_in_rect(event.button.x, event.button.y, menuButtons[2].button_rect)) {
-                                background.niveau = 1;
-                                for (int i = 0; i < MENU_BUTTONS_COUNT; i++) {
-                                    menuButtons[i].actif = 0;
-                                }
-                            } else if (point_in_rect(event.button.x, event.button.y, menuButtons[3].button_rect)) {
-                                playing = 0;
-                                for (int i = 0; i < MENU_BUTTONS_COUNT; i++) {
-                                    menuButtons[i].actif = 0;
-                                }
-                            }
-                        }
-                        break;
-            case SDL_KEYDOWN:
-                if (event.key.keysym.sym == SDLK_q) {
-                    playing = 0;
-                }
         }
-      }
-      
-      //Settings Menu handling and blitting
-      else if(background.niveau == 1){
-      		AfficherBouton(settingButtons, ecran, 1);
-        	AfficherSoundSlider(&BS, ecran);
-     		SDL_PollEvent(&event);
-                switch (event.type) {
-                  case SDL_QUIT:
-                    playing = 0;
-                    break;
-                  //Handling Buttons hovered effect sound
-                  case SDL_MOUSEMOTION:
-                	for (int i = 0; i < SETTING_BUTTONS_COUNT; i++) {
-                    		if (point_in_rect(event.motion.x, event.motion.y, settingButtons[i].button_rect)) {
-                        		if (settingButtons[i].actif == 0) {
-                            			settingButtons[i].actif = 1;
-                            			if (!menuHoverSoundPlayed) {
-                                			Mix_PlayChannel(1, menuHoverSound, 0);
-                                			menuHoverSoundPlayed = 1;
-                            			}
-                        		}
-                    		} else {
-                     		   settingButtons[i].actif = 0;
-                   		 }
-               		 }
-                  break;
-                  //Handling Buttons hovered effect + TOGGLE(FULLSCREEN/WINDOWED Modes)
-                  case SDL_MOUSEBUTTONDOWN:
-                        if (event.button.button == SDL_BUTTON_LEFT){
-                        	if (point_in_rect(event.button.x, event.button.y, settingButtons[2].button_rect)){
-                        		background.niveau = 0;
-                        	}else if(point_in_rect(event.button.x, event.button.y, settingButtons[0].button_rect)){
-                        		toggleFullScreen();
-                        	}else if(point_in_rect(event.button.x, event.button.y, settingButtons[1].button_rect)){
-                        		toggleWindowedScreen();
-                        	}
-                        	//SOUNDBUTTONS HANDLING
-                        	
-                        	if(!buttonClicked){
-                        		//VOLUME DOWN
-                        		if(point_in_rect(event.button.x, event.button.y, settingButtons[3].button_rect) && BS.soundLevel != 0){
-                        			changeBackgroundSoundLevel(&BS, 0);
-                        		}
-                        		//VOLUME UP
-                        		else if(point_in_rect(event.button.x, event.button.y, settingButtons[4].button_rect) && BS.soundLevel != 4){
-                        			changeBackgroundSoundLevel(&BS, 1);
-                        		}
-                        		//DELAY BETWEEN BUTTON CLICKS
-                        		buttonClicked = 1;
-                        		delay = 0;
-                        	}
-                        	
+
+        if(background.niveau == 0){
+            AfficherBouton(menuButtons, ecran, 0);
+            switch (event.type) {
+                case SDL_MOUSEMOTION:
+                    for (int i = 0; i < MENU_BUTTONS_COUNT; i++) {
+                        if (point_in_rect(event.motion.x, event.motion.y, menuButtons[i].button_rect)) {
+                            if (menuButtons[i].actif == 0) {
+                                menuButtons[i].actif = 1;
+                                if (!menuHoverSoundPlayed) {
+                                    Mix_PlayChannel(1, menuHoverSound, 0);
+                                    menuHoverSoundPlayed = 1;
+                                }
+                            }
+                        } else {
+                            menuButtons[i].actif = 0;
                         }
-                  break;
+                    }
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    if (event.button.button == SDL_BUTTON_LEFT) {
+                        if (point_in_rect(event.button.x, event.button.y, menuButtons[0].button_rect)) {
+                            background.niveau = 2;
+                            for (int i = 0; i < MENU_BUTTONS_COUNT; i++) {
+                                menuButtons[i].actif = 0;
+                            }
+                        } else if (point_in_rect(event.button.x, event.button.y, menuButtons[2].button_rect)) {
+                            background.niveau = 1;
+                            for (int i = 0; i < MENU_BUTTONS_COUNT; i++) {
+                                menuButtons[i].actif = 0;
+                            }
+                        } else if (point_in_rect(event.button.x, event.button.y, menuButtons[3].button_rect)) {
+                            playing = 0;
+                            for (int i = 0; i < MENU_BUTTONS_COUNT; i++) {
+                                menuButtons[i].actif = 0;
+                            }
+                        }
+                    }
+                    break;
+                case SDL_KEYDOWN:
+                    if(!keysClicked){
+                        if (event.key.keysym.sym == SDLK_DOWN) {
+                            previousButtonIndex = selectedButtonIndex;
+                            selectedButtonIndex = (selectedButtonIndex + 1) % MENU_BUTTONS_COUNT;
+                            Mix_PlayChannel(1, menuHoverSound, 0);
+                            menuButtons[selectedButtonIndex].actif = 1;
+                            menuButtons[previousButtonIndex].actif = 0;
+                        } else if (event.key.keysym.sym == SDLK_UP) {
+                            previousButtonIndex = selectedButtonIndex;
+                            selectedButtonIndex = (selectedButtonIndex - 1 + MENU_BUTTONS_COUNT) % MENU_BUTTONS_COUNT;
+                            Mix_PlayChannel(1, menuHoverSound, 0);
+                            menuButtons[selectedButtonIndex].actif = 1;
+                            menuButtons[previousButtonIndex].actif = 0;
+                        } else if (event.key.keysym.sym == SDLK_RETURN) {
+                            menuButtons[selectedButtonIndex].actif = 0;
+                            menuButtons[previousButtonIndex].actif = 0;
+                            switch (selectedButtonIndex) {
+                                case 0:
+                                    background.niveau = 2;
+                                    selectedButtonIndex = 0;
+                                    previousButtonIndex = 0;
+                                    break;
+                                case 2:
+                                    background.niveau = 1;
+                                    selectedButtonIndex = 0;
+                                    previousButtonIndex = 0;
+                                    break;
+                                case 3:
+                                    playing = 0;
+                                    break;
+                            }
+                        }
+                        keysClicked = 1;
+                    }
+                    break;
+                case SDL_KEYUP:
+                    keysClicked = 0;
+                    break;
             }
-      
-      }
-      //
-      if(delay > 50){
-      	buttonClicked = 0;
-      }
-      	delay++;
+        }
+        else if(background.niveau == 1){
+            AfficherBouton(settingButtons, ecran, 1);
+            AfficherSoundSlider(&BS, ecran);
+            switch (event.type) {
+                case SDL_MOUSEMOTION:
+                    for (int i = 0; i < SETTING_BUTTONS_COUNT; i++) {
+                        if (point_in_rect(event.motion.x, event.motion.y, settingButtons[i].button_rect)) {
+                            if (settingButtons[i].actif == 0) {
+                                settingButtons[i].actif = 1;
+                                if (!menuHoverSoundPlayed) {
+                                    Mix_PlayChannel(1, menuHoverSound, 0);
+                                    menuHoverSoundPlayed = 1;
+                                }
+                            }
+                        } else {
+                            settingButtons[i].actif = 0;
+                        }
+                    }
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    if (event.button.button == SDL_BUTTON_LEFT){
+                        if (point_in_rect(event.button.x, event.button.y, settingButtons[2].button_rect)){
+                            background.niveau = 0;
+                        }else if(point_in_rect(event.button.x, event.button.y, settingButtons[0].button_rect)){
+                            toggleFullScreen();
+                        }else if(point_in_rect(event.button.x, event.button.y, settingButtons[1].button_rect)){
+                            toggleWindowedScreen();
+                        }
+                        if(!buttonClicked){
+                            if(point_in_rect(event.button.x, event.button.y, settingButtons[3].button_rect) && BS.soundLevel != 0){
+                                changeBackgroundSoundLevel(&BS, 0);
+                            }else if(point_in_rect(event.button.x, event.button.y, settingButtons[4].button_rect) && BS.soundLevel != 4){
+                                changeBackgroundSoundLevel(&BS, 1);
+                            }
+                            buttonClicked = 1;
+                            delay = 0;
+                        }
+                    }
+                    break;
+                case SDL_KEYDOWN:
+                    if(!keysClicked){
+                        if(event.key.keysym.sym == SDLK_DOWN) {
+                            previousButtonIndex = selectedButtonIndex;
+                            selectedButtonIndex = (selectedButtonIndex + 1) % SETTING_BUTTONS_COUNT;
+                            Mix_PlayChannel(1, menuHoverSound, 0);
+                            settingButtons[selectedButtonIndex].actif = 1;
+                            settingButtons[previousButtonIndex].actif = 0;
+                        } else if (event.key.keysym.sym == SDLK_UP) {
+                            previousButtonIndex = selectedButtonIndex;
+                            selectedButtonIndex = (selectedButtonIndex - 1 + SETTING_BUTTONS_COUNT) % SETTING_BUTTONS_COUNT;
+                            Mix_PlayChannel(1, menuHoverSound, 0);
+                            settingButtons[selectedButtonIndex].actif = 1;
+                            settingButtons[previousButtonIndex].actif = 0;
+                        } else if(event.key.keysym.sym == SDLK_RETURN){
+                            settingButtons[selectedButtonIndex].actif = 0;
+                            settingButtons[previousButtonIndex].actif = 0;
+                            switch(selectedButtonIndex){
+                                case 0:
+                                    toggleFullScreen();
+                                    break;
+                                case 1:
+                                    toggleWindowedScreen();
+                                    break;
+                                case 2:
+                                    background.niveau = 0;
+                                    break;
+                            }
+                            keysClicked = 1;
+                        }
+                    }
+                    break;
+                case SDL_KEYUP:
+                    keysClicked = 0;
+                    break;
+            }
+        }
+
+        if(delay > 50){
+            buttonClicked = 0;
+        }
+        delay++;
+
         AfficherBoutonActif(menuButtons, ecran);
         menuHoverSoundPlayed = 0;
         SDL_Flip(ecran);
     }
-    
+
     FreeBackground(&background);
     FreeBouton(menuButtons, MENU_BUTTONS_COUNT);
     FreeBouton(settingButtons, SETTING_BUTTONS_COUNT);
